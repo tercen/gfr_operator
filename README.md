7f67a554a09e3d14a9538e6541087d01cd7f8ac8
infoTercen <info@tercen.com>
2018-10-07 23:30:51 +0200

    added four operators

--- README.md
+++ README.md
@@ -1,17 +1,82 @@
-# Glomerular filtration rate estimate
+# eGFR operator
 
-# Repository
+#### Description
+`eGFR` operator calculates the glomerular filtration rate.
+
+
+##### Usage
+Input projection|.
+---|---
+`col` | 2nd factor age, 3rd factor gender
+`y-axis, layer1`| is the value for serum creatine marker
+`y-axis, layer2`| is the value for serum cystatin c marker
+
+
+Output relations|.
+---|---
+`eGFR_0`| numeric, equation 0 calculation of the GFR estimate (e.g. per cell)
+`eGFR_1`| numeric, equation 1 calculation of the GFR estimate (e.g. per cell)
+`eGFR_2`| numeric, equation 2 calculation of the GFR estimate (e.g. per cell)
+`eGFR_3`| numeric, equation 3 calculation of the GFR estimate (e.g. per cell)
+
+##### Details
+`gfr` operator estimates Glomerular filtration rate (GFR) is the best overall index of kidney function.
+Four equations are used to calculate four eGFR values.
+
+equation 0:
+```r
+    a <- ifelse (gender=="Female", -0.248, -0.207) 
+    k <- ifelse (gender=="Female", 0.7, 0.9) 
+    eGFR <-  135 * (min(crt/k, 1))^a * (max(crt/k, 1))^-0.601 * (min(cyt/0.8, 1))^-0.375 * (max(cyt/0.8, 1))^-0.711 * 0.995^age
+    eGFR <- ifelse (gender=="Female",0.969 * eGFR, eGFR)
+    eGFR <- ifelse (race=="Black",1.08 * eGFR, eGFR)
 ```
-https://github.com/tercen/gfr_operator.git
+
+equation 1:
+```r
+    if (gender == "Female"){
+           if   (crt <= 0.7) { eGFR <- 144 * (crt/ 0.7)^-0.329  * 0.995^age }
+           if   (crt >  0.7) { eGFR <- 144 * (crt/ 0.7)^-1.209 * 0.995^age }
+    }
+    if (gender == "Male"){
+           if   (crt <= 0.9) { eGFR <- 144 * (crt/ 0.9)^-0.411  * 0.995^age }
+           if   (crt >  0.9) { eGFR <- 144 * (crt/ 0.9)^-1.209 * 0.995^age }
+    }
+    eGFR <- ifelse (race=="Black",1.159 * eGFR, eGFR)
 ```
 
-## Deploy
-
-```R
-packrat::init(options = list(use.cache = TRUE))
+equation 2:
+```r
+      if   (cyt <= 0.8) { eGFR <- 133 * (cyt/ 0.8)^-0.449 * 0.996^age }
+      if   (cyt >  0.8) { eGFR <- 133 * (cyt/ 0.8)^-1.328 * 0.996^age }
+      
+    eGFR <- ifelse (gender=="Female",0.932 * eGFR, eGFR)
 ```
 
-# Feedback
+equation 3:
+```r
+    if (gender == "Female"){
+      if   (crt <= 0.7 || cyt <= 0.8) { eGFR <- 130 * (crt/ 0.7)^-0.248 * (cyt/ 0.8)^-0.375 * 0.995^age }
+      if   (crt <= 0.7 || cyt >  0.8) { eGFR <- 130 * (crt/ 0.7)^-0.248 * (cyt/ 0.8)^-0.711 * 0.995^age }
+      if   (crt >  0.7 || cyt <= 0.8) { eGFR <- 130 * (crt/ 0.7)^-0.601 * (cyt/ 0.8)^-0.375 * 0.995^age }
+      if   (crt >  0.7 || cyt >  0.8) { eGFR <- 130 * (crt/ 0.7)^-0.601 * (cyt/ 0.8)^-0.711 * 0.995^age }
+    }
+    if (gender == "Male"){
+      if   (crt <= 0.9 || cyt <= 0.8) { eGFR <- 130 * (crt/ 0.7)^-0.207 * (cyt/ 0.8)^-0.375 * 0.995^age }
+      if   (crt <= 0.9 || cyt >  0.8) { eGFR <- 130 * (crt/ 0.7)^-0.207 * (cyt/ 0.8)^-0.711 * 0.995^age }
+      if   (crt >  0.9 || cyt <= 0.8) { eGFR <- 130 * (crt/ 0.7)^-0.601 * (cyt/ 0.8)^-0.375 * 0.995^age }
+      if   (crt >  0.9 || cyt >  0.8) { eGFR <- 130 * (crt/ 0.7)^-0.601 * (cyt/ 0.8)^-0.711 * 0.995^age }
+    }
+      eGFR <- ifelse (race=="Black",1.159 * eGFR, eGFR)
+```
 
-# Documentation
-see the doc directory
+
+#### References
+ 
+ref for equation 0: [https://www.kidney.org/professionals/kdoqi/gfr_calculator]
+
+
+##### See Also
+
+
+#### Examples
--- main.R
+++ main.R
@@ -1,6 +1,63 @@
 library(tercen)
 library(dplyr)
 
+calc_egfr <- function(crt, cyt, gender, age, race) {
+  
+  eGFR_0 <- {
+    eGFR <- NULL 
+    a <- ifelse (gender=="Female", -0.248, -0.207) 
+    k <- ifelse (gender=="Female", 0.7, 0.9) 
+    
+    eGFR <-  135 * (min(crt/k, 1))^a * (max(crt/k, 1))^-0.601 * (min(cyt/0.8, 1))^-0.375 * (max(cyt/0.8, 1))^-0.711 * 0.995^age
+    eGFR <- ifelse (gender=="Female",0.969 * eGFR, eGFR)
+    eGFR <- ifelse (race=="Black",1.08 * eGFR, eGFR)
+    eGFR
+  }
+  
+  eGFR_1 <- {
+    eGFR <- NULL 
+    if (gender == "Female"){
+      if   (crt <= 0.7) { eGFR <- 144 * (crt/ 0.7)^-0.329  * 0.995^age }
+      if   (crt >  0.7) { eGFR <- 144 * (crt/ 0.7)^-1.209 * 0.995^age }
+    }
+    if (gender == "Male"){
+      if   (crt <= 0.9) { eGFR <- 144 * (crt/ 0.9)^-0.411  * 0.995^age }
+      if   (crt >  0.9) { eGFR <- 144 * (crt/ 0.9)^-1.209 * 0.995^age }
+    }
+    eGFR <- ifelse (race=="Black",1.159 * eGFR, eGFR)
+    eGFR
+  }
+  
+  eGFR_2 <- {
+    eGFR <- NULL 
+    if   (cyt <= 0.8) { eGFR <- 133 * (cyt/ 0.8)^-0.449 * 0.996^age }
+    if   (cyt >  0.8) { eGFR <- 133 * (cyt/ 0.8)^-1.328 * 0.996^age }
+    
+    eGFR <- ifelse (gender=="Female",0.932 * eGFR, eGFR)
+    eGFR
+  }
+  
+  eGFR_3 <- {
+    eGFR <- NULL  
+    if (gender == "Female"){
+      if   (crt <= 0.7 || cyt <= 0.8) { eGFR <- 130 * (crt/ 0.7)^-0.248 * (cyt/ 0.8)^-0.375 * 0.995^age }
+      if   (crt <= 0.7 || cyt >  0.8) { eGFR <- 130 * (crt/ 0.7)^-0.248 * (cyt/ 0.8)^-0.711 * 0.995^age }
+      if   (crt >  0.7 || cyt <= 0.8) { eGFR <- 130 * (crt/ 0.7)^-0.601 * (cyt/ 0.8)^-0.375 * 0.995^age }
+      if   (crt >  0.7 || cyt >  0.8) { eGFR <- 130 * (crt/ 0.7)^-0.601 * (cyt/ 0.8)^-0.711 * 0.995^age }
+    }
+    if (gender == "Male"){
+      if   (crt <= 0.9 || cyt <= 0.8) { eGFR <- 130 * (crt/ 0.7)^-0.207 * (cyt/ 0.8)^-0.375 * 0.995^age }
+      if   (crt <= 0.9 || cyt >  0.8) { eGFR <- 130 * (crt/ 0.7)^-0.207 * (cyt/ 0.8)^-0.711 * 0.995^age }
+      if   (crt >  0.9 || cyt <= 0.8) { eGFR <- 130 * (crt/ 0.7)^-0.601 * (cyt/ 0.8)^-0.375 * 0.995^age }
+      if   (crt >  0.9 || cyt >  0.8) { eGFR <- 130 * (crt/ 0.7)^-0.601 * (cyt/ 0.8)^-0.711 * 0.995^age }
+    }
+    eGFR <- ifelse (race=="Black",1.159 * eGFR, eGFR)
+    eGFR 
+  }
+  return(list(eGFR_0,eGFR_1, eGFR_2, eGFR_3))
+}
+
+
 do.eGFR = function(df) {
   
   ci <- df$.ci[1]
@@ -25,26 +82,15 @@ do.eGFR = function(df) {
   if (length(cyt) == 0) cyt = NaN
   
   # convert units of crt
-  crt <- crt/ 88.4
-  
-  a <- ifelse (gender=="Female",-0.248,-0.207) 
-  k <- ifelse (gender=="Female",0.7,0.9) 
-  
-  
-  eGFR <-
-    135 * (min(crt/k, 1))^a *
-    (max(crt/k, 1))^-0.601 *
-    (min(cyt/0.8, 1))^-0.375 *
-    (max(cyt/0.8, 1))^-0.711 *
-    0.995^age
-  
-  eGFR <- ifelse (gender=="Female",0.969 * eGFR,eGFR)
-  eGFR <- ifelse (race=="Black",1.08 * eGFR,eGFR)
-  
+ if (as.logical(ctx$op.value('as uM/l'))) crt <- crt/ 88.4
+  eGFR    <- calc_egfr(crt,cyt, gender, age, race)
   return(data.frame(
     .ri = df$.ri[1],
     .ci = df$.ci[1],
-    eGFR = eGFR
+    eGFR_0 = eGFR[[1]],
+    eGFR_1 = eGFR[[2]],
+    eGFR_2 = eGFR[[3]],
+    eGFR_3 = eGFR[[4]]
   ))
 }
 
@@ -53,9 +99,20 @@ ctx = tercenCtx()
 if (nrow(unique(ctx$select(c('.axisIndex')))) != 2)
   stop("Two layers are required, the first with the creatinine value and the second with the cystatin")
 
+if (ncol(ctx$cselect() < 3)) stop ("Require age and gender as 2nd and 3rd factors on columns")
+all_ages <- ctx$cselect()[[2]]
+all_genders <- ctx$cselect()[[3]]
+
+check_ages_within_range <- (all(unlist(lapply(all_ages, function(x) x %in% 1:100))))
+check_gender_values <- (all(unlist(lapply(all_genders, function(x) x %in% c("Male", "Female")))))
+
+if (!check_ages_in_range)  stop ("ages required as the second factor on columns")
+if (!check_gender_values)  stop ("gender required as the third factor on columns")
+
 df <- ctx %>%
   select(.ci, .ri, .y, .axisIndex) %>%
   group_by(.ci, .ri) %>%
   do(do.eGFR(.)) %>%
   ctx$addNamespace() %>%
   ctx$save()
+
